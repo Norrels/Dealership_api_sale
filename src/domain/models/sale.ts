16b24b7d7dc1ab0d7a08ddfeddc42cb65d1cb05a
Matheus Zacarias <matheuus412@gmail.com>
2025-11-07 19:23:41 -0300

    feat: model

---
+++ src/domain/models/sale.ts
@@ -0,0 +1,12 @@
+export interface Sale {
+  id: string;
+  vehicleId: string;
+  saleDate: Date;
+  make: string;
+  model: string;
+  year: number;
+  vin: string;
+  price: string;
+  color: string;
+  status: string;
+}
