591e0f8f1882d683727cf933ef476abd10df0a7a
Matheus Zacarias <matheuus412@gmail.com>
2025-11-08 16:28:36 -0300

    feat: get vehicle by id

--- src/application/service/vehicleService.ts
+++ src/application/service/vehicleService.ts
@@ -1,10 +1,14 @@
-import { VehicleRepository } from "../../domain/ports/out/vehicleRepositoryPort";
+import { VehicleRepositoryPort } from "../../domain/ports/out/vehicleRepositoryPort";
 import { Vehicle } from "../../domain/models/vehicle";
 
 export class VehicleService {
-  constructor(private vehicleRepository: VehicleRepository) {}
+  constructor(private vehicleRepository: VehicleRepositoryPort) {}
 
   async getAllAvailableVehicles(): Promise<Vehicle[]> {
     return await this.vehicleRepository.getAllAvailableVehicles();
+  }
+
+  async getVehicleById(id: string): Promise<Vehicle | undefined> {
+    return await this.vehicleRepository.getVehicleById(id);
   }
 }
